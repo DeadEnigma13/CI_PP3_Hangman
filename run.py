af9594b131061f3e18cfd9fec9c5c441a776d6e1
Zack Owen <zackowen15@gmail.com>
2022-10-04 14:10:51 +0000

    Initial commit and add game title

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time
+from time import sleep
+import random
+from colors import Color as Col
+
+
+def game_logo():
+    """
+    Displays Game Title
+    """
+    print(Col.BLUE + "Hello, Welcome To:")
+    print(" ")
+    print(Col.LOGO_R + " _    _")
+    print(Col.LOGO_R + "| |  | |")
+    print(Col.LOGO_R + "| |  | |")
+    print(Col.LOGO_R + "| |  | |      _       ___      _   ____")
+    print(Col.LOGO_R + "| |__| |     / \     |   \    | | / ___\ ")
+    print(Col.LOGO_R + "|  __  |    / _ \    | |\ \   | |/ /")
+    print(Col.LOGO_R + "| |  | |   / /_\ \   | | \ \  | || |  ___")
+    print(Col.LOGO_R + "| |  | |  /  ___  \  | |  \ \ | || | |__ |")
+    print(Col.LOGO_R + "| |  | | /  /   \  \ | |   \ \| |\  \__/ /")
+    print(Col.LOGO_R + "|_|  |_|/__/     \__\|_|    \___| \_____/")
+    print(Col.LOGO_R + " ___      ___       _       ___      _")
+    print(Col.LOGO_R + "|   \    /   |     / \     |   \    | |")
+    print(Col.LOGO_R + "| |\ \  / /| |    / _ \    | |\ \   | |")
+    print(Col.LOGO_R + "| | \ \/ / | |   / /_\ \   | | \ \  | |")
+    print(Col.LOGO_R + "| |  \__/  | |  /  ___  \  | |  \ \ | |")
+    print(Col.LOGO_R + "| |        | | /  /   \  \ | |   \ \| |")
+    print(Col.LOGO_R + "|_|        |_|/__/     \__\|_|    \___|")
+    print(" ")
+    print(" ")
+    print(" ")
+    time.sleep(1)
