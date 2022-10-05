2f32ec0885c4e787ea1da08f26f0974bcb212310
Zack Owen <zackowen15@gmail.com>
2022-10-05 16:37:01 +0000

    Fixed TypeError: '>=' not supported between instances of 'list' and 'int'

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
-import time
-from time import sleep
 import random
-from colors import Color as Col
-
 
 MAX_TURNS = 10
 game_running = True
@@ -16,116 +12,6 @@ word_bank = ["constitutional", "river", "president", "avenue", "game",
 """
 Choose a random word
 """
-
-
-def game_logo():
-    """
-    Displays Game Title
-    """
-    print(Col.BLUE + "Hello, Welcome To:")
-    print(" ")
-    print(Col.LOGO_R + " _    _")
-    print(Col.LOGO_R + "| |  | |")
-    print(Col.LOGO_R + "| |  | |")
-    print(Col.LOGO_R + "| |  | |      _       ___      _   ____")
-    print(Col.LOGO_R + "| |__| |     / \     |   \    | | / ___\ ")
-    print(Col.LOGO_R + "|  __  |    / _ \    | |\ \   | |/ /")
-    print(Col.LOGO_R + "| |  | |   / /_\ \   | | \ \  | || |  ___")
-    print(Col.LOGO_R + "| |  | |  /  ___  \  | |  \ \ | || | |__ |")
-    print(Col.LOGO_R + "| |  | | /  /   \  \ | |   \ \| |\  \__/ /")
-    print(Col.LOGO_R + "|_|  |_|/__/     \__\|_|    \___| \_____/")
-    print(Col.LOGO_R + " ___      ___       _       ___      _")
-    print(Col.LOGO_R + "|   \    /   |     / \     |   \    | |")
-    print(Col.LOGO_R + "| |\ \  / /| |    / _ \    | |\ \   | |")
-    print(Col.LOGO_R + "| | \ \/ / | |   / /_\ \   | | \ \  | |")
-    print(Col.LOGO_R + "| |  \__/  | |  /  ___  \  | |  \ \ | |")
-    print(Col.LOGO_R + "| |        | | /  /   \  \ | |   \ \| |")
-    print(Col.LOGO_R + "|_|        |_|/__/     \__\|_|    \___|")
-    print(" ")
-    print(" ")
-    print(" ")
-    time.sleep(1)
-
-
-def seperate_lines():
-    """
-    Print '-' lines to seperate messages
-    """
-    print(" ")
-    print("- "*30)
-    print(" ")
-
-
-def main_screen() -> str:
-    """
-    The game will load two possible options for the
-    user to select from, view game rules or start
-    game
-    """
-    time.sleep(1)
-    print(Col.BLUE + "Pick an option to continue...")
-    continue_options = "1). View rules of game\n2). Play Game\n"
-    continue_options_selected = input(continue_options)
-    seperate_lines()
-
-    # Confirm if answer is either 1 or 2
-    while continue_options_selected not in ("1", "2"):
-        print(Col.YELLOW + "Please choose option one or 2 to move on:")
-        continue_options_selected = input(continue_options)
-        seperate_lines()
-
-    if continue_options_selected == "1":
-        game_logo()
-        rules()
-
-    elif continue_options_selected == "2":
-        start_hangman()
-
-    return continue_options_selected
-
-
-def rules():
-    """
-    Display rules of the game which
-    user can exit by pressing any key
-    """
-    print(Col.RED + "Game Rules:")
-    print("The rules of the game are simple, a random word will be " +
-          "generated simply type in any letter to guess the word")
-    time.sleep(1)
-    print("Keep guessing a letter until you have" +
-          "guessed the word or ran out of guesses")
-    time.sleep(1)
-    print(" ")
-    seperate_lines()
-    input("Press any key to exit...\n")
-    main_screen()
-
-
-def start_hangman() -> str:
-    """
-    The game will check if user has played before
-    """
-    time.sleep(1)
-    print(Col.GREEN + "Have you played this game before?")
-    answer = "1). Yes \n2). No\n"
-    answered = input(answer)
-    seperate_lines()
-
-    # Confirm if answer is either Yes or No
-    while answered not in ("1", "y", "2", "n"):
-        print(Col.CYAN + "Please select one of the options shown:")
-        answered = input(answer)
-
-        seperate_lines()
-
-    if answered == "1" or answered == "y":
-        game_logo()
-
-    elif answered == "2" or answered == "n":
-        game_logo()
-
-    return answered
 
 
 def show_hangman(wrong):
@@ -211,7 +97,7 @@ def show_hangman(wrong):
         print("   ___")
 
 
-def show_word(pickedLetters):
+def show_word(pickedletters):
     """
     Display the already picked letters and how many
     guesses left
@@ -219,13 +105,13 @@ def show_word(pickedLetters):
     counter = 0
     correct_letters = 0
     for char in random_word:
-        if (char in pickedLetters):
+        if (char in pickedletters):
             print(random_word[counter].upper(), end=" ")
             correct_letters += 1
         else:
             print(" ", end=" ")
         counter += 1
-    return pickedLetters
+    return correct_letters
 
 
 def lines():
@@ -263,6 +149,8 @@ Running the game
 if __name__ == "__main__":
     while (game_running):
         random_word = random.choice(word_bank)
+        print("Welcome to Hangman, let's play!")
+        print("_____________________________________")
         show_hangman(0)
         for x in random_word:
             print("_ ", end="")
@@ -273,9 +161,7 @@ if __name__ == "__main__":
         letters_right = 0
         carry_on_playing = "no"
 
-        while (times_wrong != MAX_TURNS and
-               letters_right != word_length):
-
+        while (times_wrong != MAX_TURNS and letters_right != word_length):
             """
             Show users possible letter inputs
             """
@@ -304,17 +190,16 @@ if __name__ == "__main__":
             elif times_wrong >= MAX_TURNS:
                 print("\n\n You have lost! The word was '" + random_word +
                       "', better luck next time!\n")
-
+            """
+            Allow user to restart game or start with a new word
+            """
+            play_again = verify_input("\n Do you want to play again? (Y/N): ",
+                                      ['y', 'yes', 'yeah', 'n', 'no'])
+            if play_again in ['y', 'yes', 'yeah']:
+                game_running = True
+            else:
+                game_running = False
         """
-        Allow user to restart game or start with a new word
+        Game ending message
         """
-        play_again = verify_input("\n Do you want to play again? (Y/N) ",
-                                  ['y', 'yes', 'yeah', 'n', 'no', 'nope'])
-        if play_again in ['y', 'yes', 'yeah']:
-            game_running = True
-        else:
-            game_running = False
-    """
-    Game ending message
-    """
-    print("You selected no, game over! Hope you play again soon")
+        print("You selected no, game over! Hope you play again soon")
