9b288a5065c6a68e15867faf4770b346dcf1b7c9
Zack Owen <zackowen15@gmail.com>
2022-10-11 14:58:24 +0000

    Remove email validator

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import random
 from colors import Color as Col
+import validation as val
 
 
 MAX_TURNS = 10
@@ -14,20 +15,6 @@ word_bank = ["constitutional", "river", "president", "avenue", "game",
 """
 Choose a random word
 """
-
-
-def email_validation(x):
-    a = 0
-    y = len(x)
-    dot = x.find(".")
-    at = x.find("@")
-    for i in range(0, at):
-        if ((x[i] >= 'a' and x[i] <= 'z') or (x[i] >= 'A' and x[i] <= 'Z')):
-            a = a+1
-    if (a > 0 and at > 0 and (dot-at) > 0 and (dot+1) < y):
-        print("Valid Email")
-    else:
-        print("Invalid Email")
 
 
 def show_hangman(wrong):
@@ -180,7 +167,6 @@ def main_screen() -> str:
         start_hangman()
 
     return continue_options_selected
-    email_validation(x)
 
 
 def rules():
@@ -216,6 +202,7 @@ def start_hangman():
         rules()
 
     return answered
+
 
 """
 Running the game
